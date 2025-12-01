2edbccc38d876d67ff96f60a3aaed3a4d1d5e6ce
mahmoud Attia <51999205+mamoda@users.noreply.github.com>
2025-12-01 20:53:12 +0200

    تحديث

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
-const cors = require('cors');
+const cors = require('cors'); //cors for cross-origin requests 
 
 // Load env vars
 dotenv.config();
